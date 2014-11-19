356c63742f6c1ec44b99d07f8fc82b0be682e513
runix <rubenheavymetal@gmail.com>
2014-11-19 13:06:06 +0100

    added shuffle mode, and improve the methd for distinguish between arduino and a posix pc

--- player.c
+++ player.c
@@ -1,15 +1,30 @@
 #include "songs.h"
 #include <string.h>
 
-#if  __GNUC__
-#if  __x86_64__
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#endif
+//Defining arduino flags.
+//Change your pc architecture and compiler if it doesn't match
+//with  __GNUC__ && __x86_64__
+//Or simply, put only the ARDUINE FLAG
+//without the if preprocessor instructionw
+
+//The porpouse of this is for being
+//able to compile and test the program
+//without an arduino. Just print
+//the notes and duration
+//Modify the main method for 
+//other behaviours.
+#if  __GNUC__ && __x86_64__
+	#define ARDUINE 0
+	#include <stdio.h>
+	#include <unistd.h>
+	#include <stdlib.h>
+	#include <time.h>
+	#include "StringFormat.h"
+#else
+	#define ARDUINE 1
 #endif
 
+//Defining constants
 #define START_STOP_PIN 2
 #define NEXT_SONG_PIN 3
 #define BUZE_PIN A1
@@ -18,49 +33,161 @@
 #define SWITCH_CHANGE LOW
 #define UPDATE_SONG_NAME 1
 #define MAX_NAME_SIZE 25
+#define DELAY_BETWEEN_SONGS 2000
+
+
 //Defining types
 
+//Program state
 typedef enum {
 	RUN = 1,
 	STOP = 0
 } Player_state;
 
+typedef enum {
+	NORMAL = 1,
+	RANDOM = 2
+} Player_behaviour;
 
 //Attributes
 
+//ActualSong (pointing to the actual note)
 int *actualMusic;
-int length;
-int state;
-int lastChangeStateMilli;
-int lastChangeSongMilli;
-int *songs[] =
-	{ starWars, simpsons, indiana, knighRider, misionImp, topGun, bond };
 
-int numberOfSongs = 7;
+//Last update (Player_state) change
+//Wait a few time between changes in order
+//to avoid the noisy in the circuit
+int lastChangeStateMilli;
+
+//Last song change
+//Wait a few time between changes in order
+//to avoid the noisy in the circuit
+int lastChangeSongMilli;
+
+//Array of songs
+//Be free of add or remove songs
+//Be sure
+int *songs[] =
+	{ starWars, simpsons, indiana, knighRider, misionImp, topGun, bond};
+
+//The number of songs sizeof(songs)/sizeof(int*)
+unsigned long numberOfSongs;
+
+//The number of the actualSong
 int actualSong = 0;
+
+//The state of the player, by default STOP.
 Player_state player_state = STOP;
+
+//The max song name's length
 char actualSongName[MAX_NAME_SIZE];
 
+//The next song behaviour
+Player_behaviour player_behaviour = RANDOM;
 
 // functions signature
+
+/**
+ *
+ *setup method, set up the arduino pins and load the first song. 
+ * If ARDUINE is 0, only load the first song
+ */
 void setup(void);
+
+/**
+ * Play the actual note. If the song doesn't have
+ * more notes return 0, else 1.
+ *
+ * Arduine : buzze and delay
+ * Not arduine : just print the tone and delay
+ *
+ */
 int playActualNote(void);
+
+/**
+ * Load the song pointed
+ * by songs[actualSong % numberOfSongs]
+ * And updates the song name
+ * The max song's length is defined by MAX_NAME_SIZE 
+ */
 void loadSong(void);
+
+/**
+ * Play the actual note. If the song has'nt more notes
+ * load the next song.
+ *
+ * If ARDUINE, listen for stop/start and nextSong events
+ *
+ */
 void loop(void);
+
+/**
+ * Point to the next song
+ * If the list is ended
+ * point to the first. You
+ * can change this behaviohur
+ * and add shuffle or whatever
+ * you want. Just make sure that
+ * actualSong is in the bounds of the array
+ * of songs
+ */
 void nextSong(void);
+
+/**
+ * Updates the actual song's name
+ *
+ */
 void updateSongName();
+
+/**
+ * Do a delay of millis
+ *
+ */
+void doDelay(int millis);
+
+/**
+ * Returns a random int
+ * between 0 and max
+ *
+ */
+int randomValue(int max);
+
+
+
+//FUNCTIONS BODY
+
+int randomValue(int max) {
+#if ARDUINE
+	return ramdom(max);
+#else
+	return rand() % max;
+#endif
+}
+
+void doDelay(int millis) {
+#if ARDUINE
+	delay(millis);
+#else
+	struct timespec ts;
+	ts.tv_sec = 0;
+	ts.tv_nsec = millis*1000000;
+	nanosleep(&ts, NULL);
+#endif
+}
 
 
 void setup()
 {
-#if ! __GNUC__
-#if ! __x86_64__
+numberOfSongs = sizeof(songs)/sizeof(int*);
+#if ARDUINE
 	pinMode(START_STOP_PIN, INPUT);
 	pinMode(NEXT_SONG_PIN, INPUT);
 	pinMode(BUZE_PIN, INPUT);
 	lastChangeStateMilli = millis();
 	lastChangeSongMilli = millis();
-#endif
+        randomSeed(analogRead(0));
+#else
+	srand(time(NULL));
 #endif
 loadSong();
 }
@@ -72,17 +199,12 @@ int playActualNote()
 		int tone = ((actualNote >> 16) & 0xFFFF);
 		int duration = (actualNote & 0xFFFF);
 		if (tone != 0) {
-#if ! __GNUC__
-#if ! __x86_64__
+#if ARDUINE
 			tone(BUZE_PIN, tone);
 #endif
-#endif
 		}
-#if ! __GNUC__
-#if ! __x86_64__
-		delay(duration);
-#endif
-#else
+		doDelay(duration);
+#if ! ARDUINE
 		printf("tone : %d, duration : %d \n", tone, duration);
 #endif
 		actualMusic++;
@@ -103,18 +225,21 @@ void loadSong()
 		actualMusic++;
 	}
 	updateSongName();
+#if ! ARDUINE
+	printf("\n\n\n%ssong %s %s\n\n\n",GREEN,actualSongName,RESET);
+#endif
 }
 
 void updateSongName() {	
 	int* song = songs[actualSong % numberOfSongs];
 	if(*song == 0) {
 		song++;//Pointing to the first number
-		int sizeName = 0;
+		int sizeName = 0; 
 		int size = 0;
-		while(*song != 0) {
+		while(*song != 0 && size < MAX_NAME_SIZE ) {
 			int actualChunck = *song;
 			int i;
-			for(i = 0; i < 4; i++) {
+			for(i = 0; i < 4 && size < MAX_NAME_SIZE ; i++) {
 				const char actualChar = (char) actualChunck % 0xFF;
 				actualSongName[size++] = actualChar;
 				actualChunck = actualChunck >> 8;	
@@ -129,8 +254,7 @@ void updateSongName() {
 
 void loop()
 {
-#if ! __GNUC__
-#if ! __x86_64__
+#if ARDUINE
 	if (digitalRead(START_STOP_PIN) == SWITCH_CHANGE
 		&& (millis() - lastChangeStateMilli) > TIME_ON_STATE_CHANGE) {
 		if (player_state == RUN) {
@@ -147,31 +271,29 @@ void loop()
 		lastChangeSongMilli = millis();
 	}
 #endif
-#endif
 	if (player_state == RUN) {
 		int state = playActualNote();
 		if (state == 0) {
 			nextSong();
 		}
 	} else {
-#if ! __GNUC__
-#if ! __x86_64__
-		delay(DELAY_ON_STOP);
-#endif
-#endif
+		doDelay(DELAY_ON_STOP);
 	}
 
 }
 
 void nextSong()
 {
-	actualSong++;
+	if(player_behaviour == RANDOM) {
+		actualSong = randomValue(numberOfSongs);
+	} else {
+		actualSong++;
+	}
+	doDelay(DELAY_BETWEEN_SONGS);
 	loadSong();
-
 }
 
-#if  __GNUC__
-#if  __x86_64__
+#if ! ARDUINE
 int main(int argc, char **argv)
 {
 	setup();
@@ -180,5 +302,4 @@ int main(int argc, char **argv)
 		loop();
 	}
 }
-#endif
 #endif
